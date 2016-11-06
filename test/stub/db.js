2da7801ff2dc5c8ab975dc58302769935cb61cd1
Hector Flores <hecto932@gmail.com>
2016-11-06 02:46:14 -0600

    Creacion de nuestro endpoint POST /user

--- test/stub/db.js
+++ test/stub/db.js
@@ -33,4 +33,8 @@ export default class Db {
   getImagesByTag (tag) {
     return Promise.resolve(fixtures.getImagesByTag())
   }
+
+  saveUser (user) {
+    return Promise.resolve(fixtures.getUser())
+  }
 }
--- users.js
+++ users.js
@@ -1,19 +1,31 @@
 'use strict'
 
-import { send, json } from 'micro' // eslint-disable-line no-unused-vars
+import { send, json } from 'micro'
 import HttpHash from 'http-hash'
 import Db from 'platzigram-db'
 import config from './config'
 import DbStub from './test/stub/db'
 
 const env = process.env.NODE_ENV || 'production'
-let db = new Db(config.db) // eslint-disable-line no-unused-vars
+let db = new Db(config.db)
 
 if (env === 'test') {
   db = new DbStub()
 }
 
 const hash = HttpHash()
+
+hash.set('POST /', async function saveUser (req, res, params) {
+  let user = await json(req)
+  await db.connect()
+  let created = await db.saveUser(user)
+  await db.disconnect()
+
+  delete created.email
+  delete created.password
+
+  send(res, 201, created)
+})
 
 export default async function main (req, res) {
   let { method, url } = req
